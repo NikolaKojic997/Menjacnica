6a79a240b3fd5bbf6abedb6cf86218d8ffccc53b
NikolaKojic997 <volimdahodam@gmail.com>
2018-03-14 08:25:19 +0100

    Dodate equals, hashCode i toString metode u klase

--- Menjacnica/src/Menjacnica/Menjacnica.java
+++ Menjacnica/src/Menjacnica/Menjacnica.java
@@ -25,6 +25,39 @@ public class Menjacnica {
 	public void setValute(LinkedList<Valuta> valute) {
 		this.valute = valute;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((adresa == null) ? 0 : adresa.hashCode());
+		result = prime * result + ((ime == null) ? 0 : ime.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Menjacnica other = (Menjacnica) obj;
+		if (adresa == null) {
+			if (other.adresa != null)
+				return false;
+		} else if (!adresa.equals(other.adresa))
+			return false;
+		if (ime == null) {
+			if (other.ime != null)
+				return false;
+		} else if (!ime.equals(other.ime))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Menjacnica [ime=" + ime + ", adresa=" + adresa + ", valute=" + valute + "]";
+	}
 	
 	
 	
