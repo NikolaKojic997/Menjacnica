4df2f7c33f2766514a4b7032dc4375c734053e8d
NikolaKojic997 <volimdahodam@gmail.com>
2018-03-14 08:13:44 +0100

    Napravljene klase Menjacnica i valuta, get i set bez kontrole

---
+++ Menjacnica/src/Menjacnica/Menjacnica.java
@@ -0,0 +1,32 @@
+package Menjacnica;
+
+import java.util.LinkedList;
+
+public class Menjacnica {
+	
+	private String ime;
+	private String adresa;
+	private LinkedList<Valuta> valute;
+	public String getIme() {
+		return ime;
+	}
+	public void setIme(String ime) {
+		this.ime = ime;
+	}
+	public String getAdresa() {
+		return adresa;
+	}
+	public void setAdresa(String adresa) {
+		this.adresa = adresa;
+	}
+	public LinkedList<Valuta> getValute() {
+		return valute;
+	}
+	public void setValute(LinkedList<Valuta> valute) {
+		this.valute = valute;
+	}
+	
+	
+	
+	
+}
