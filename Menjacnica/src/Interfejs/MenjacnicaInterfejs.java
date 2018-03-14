33df5bbe9536067886d61d56a9d81711f3033010
NikolaKojic997 <volimdahodam@gmail.com>
2018-03-14 10:25:13 +0100

    Ispravljena greska kod specifikacije interfejsa

--- Menjacnica/src/Interfejs/MenjacnicaInterfejs.java
+++ Menjacnica/src/Interfejs/MenjacnicaInterfejs.java
@@ -2,10 +2,12 @@ package Interfejs;
 
 import java.util.GregorianCalendar;
 
+import Menjacnica.Valuta;
+
 public interface MenjacnicaInterfejs {
 	
 	public void dodajKurs(String naziv,String skraceniNaziv,GregorianCalendar datum,  double prodajniKurs, double kupovniKurs); 
 	public void obrusiKurs(String skraceniNaziv, GregorianCalendar datum); 
-	public double vratiKurs(String skraceniNaziv, GregorianCalendar datum);
+	public Valuta vratiKurs(String skraceniNaziv, GregorianCalendar datum);
 	
 }
